f6c11fcd8242521d07b46d10c13b6c7abb26866c
Guilherme Albuquerque <guilhermetstsms@gmail.com>
2022-01-21 09:47:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let contador = 1
+let contador2 = 10
+
+for (let contador = 1; contador <= 10; contador +=1) {
+    console.log(contador)
+}
+
+while (contador2 >= 1) {
+    console.log(contador2);
+    contador2 -=1
+}
+
+for(let cont = 1; cont <= 100; cont += 2){
+    console.log(cont);
+}
+
+while (contador <=10) {
+    console.log(contador);
+    contador += 2
+}
